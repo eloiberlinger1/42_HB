85a99125c2639332948597459c3ffd4cf9df631f
Eloi Berlinger <eloi.berling@protonmail.com>
2025-11-10 12:21:45 +0100

    norm

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,11 @@
 /*   By: eberling <eberling@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/06 09:37:49 by eberling          #+#    #+#             */
-/*   Updated: 2025/11/07 13:20:03 by eberling         ###   ########.fr       */
+/*   Updated: 2025/11/10 12:21:28 by eberling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
 
 static int	contains(char c, char *set)
 {
@@ -21,74 +20,74 @@ static int	contains(char c, char *set)
 	return (0);
 }
 
-static char *save_buffer(char *result, char *buffer, size_t buffer_size)
+static char	*save_buffer(char *result, char *buffer, size_t buffer_size)
 {
-    char *temp;
-    size_t result_len;
-    size_t  i;
+	char	*temp;
+	size_t	result_len;
+	size_t	i;
 
-    result_len = 0;
-    if(result != NULL)
-        while (result[result_len])
-            result_len++;
-    temp = malloc(result_len + buffer_size + 1);
-    if (temp == NULL)
-        return (NULL);
-    i = -1;
-    while (++i < result_len)
-        temp[i] = result[i];
-    i = -1;
-    while (++i < buffer_size)
-    {
-        if (buffer[i] == '\n')
-            break;
-        temp[result_len + i] = buffer[i];
-    }
-    free (result);
-    return (temp);
+	result_len = 0;
+	if (result != NULL)
+		while (result[result_len])
+			result_len++;
+	temp = malloc(result_len + buffer_size + 1);
+	if (temp == NULL)
+		return (NULL);
+	i = -1;
+	while (++i < result_len)
+		temp[i] = result[i];
+	i = -1;
+	while (++i < buffer_size)
+	{
+		if (buffer[i] == '\n')
+			break ;
+		temp[result_len + i] = buffer[i];
+	}
+	free(result);
+	return (temp);
 }
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-    size_t     buffer_size;
-    char    *buffer;
-    char    *result;
+	size_t	buffer_size;
+	char	*buffer;
+	char	*result;
 
-    buffer_size = BUFFER_SIZE;
-    result = NULL;
-    buffer = malloc(buffer_size);
-    while (1)
-    {
-        if (buffer_size == 0)
-        {
-            buffer_size = BUFFER_SIZE;
-            result = save_buffer(result, buffer, buffer_size);
-            if (contains('\n', buffer))
-                break;
-        }
-        buffer_size -= read(fd, buffer, buffer_size);
-    }
-    free(buffer);
-    return (result);
+	buffer_size = BUFFER_SIZE;
+	result = NULL;
+	buffer = malloc(buffer_size);
+	while (1)
+	{
+		if (buffer_size == 0)
+		{
+			buffer_size = BUFFER_SIZE;
+			result = save_buffer(result, buffer, buffer_size);
+			if (contains('\n', buffer))
+				break ;
+		}
+		buffer_size -= read(fd, buffer, buffer_size);
+	}
+	free(buffer);
+	return (result);
 }
 
-int main(void)
+int	main(void)
 {
-    int fd;
-    fd = open("test.txt", O_RDONLY);
-    
-    if (fd == -1)
-    {
-        perror("Erreur lors de l'ouverture du fichier");
-        return (1);
-    }
-    printf("\ngetnextlien result1 : \n\n%s\n\n", get_next_line(fd));
-    printf("\ngetnextlien result2 : \n\n%s\n\n", get_next_line(fd));
-    if (close(fd) == -1)
-    {
-        perror("Erreur lors de la fermeture du fichier");
-        return (1);
-    }
-    printf("Termine.\n");
-    return (0);
+	int fd;
+	fd = open("test.txt", O_RDONLY);
+
+	if (fd == -1)
+	{
+		perror("Erreur lors de l'ouverture du fichier");
+		return (1);
+	}
+	printf("\ngetnextlien result1 : \n\n%s\n\n", get_next_line(fd));
+	printf("\ngetnextlien result2 : \n\n%s\n\n", get_next_line(fd));
+	if (close(fd) == -1)
+	{
+		perror("Erreur lors de la fermeture du fichier");
+		return (1);
+	}
+	printf("Termine.\n");
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: eberling <eberling@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/06 09:40:12 by eberling          #+#    #+#             */
-/*   Updated: 2025/11/07 13:20:34 by eberling         ###   ########.fr       */
+/*   Updated: 2025/11/10 12:21:35 by eberling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 
 # define BUFFER_SIZE 5
 
+# include <fcntl.h>
 # include <stdarg.h>
 # include <stdint.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include <fcntl.h>
-char *get_next_line(int fd);
+char	*get_next_line(int fd);
 
 #endif
