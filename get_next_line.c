fac4c37478c3f24e0b7b04250065b41fcf40bd87
Eloi Berlinger <eloi.berling@protonmail.com>
2025-11-12 09:48:09 +0100

    UP

--- get_next_line.c
+++ get_next_line.c
@@ -6,55 +6,12 @@
 /*   By: eberling <eberling@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/06 09:37:49 by eberling          #+#    #+#             */
-/*   Updated: 2025/11/12 08:55:46 by eberling         ###   ########.fr       */
+/*   Updated: 2025/11/12 09:47:39 by eberling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	contains(char c, char *set)
-{
-	if (set == NULL)
-		return (0);
-	while (*set)
-		if (c == *set++)
-			return (1);
-	return (0);
-}
-
-int	ft_strlen(const char *s)
-{
-	int	i;
-
-	i = 0;
-	while (s[i] != '\0')
-		i++;
-	return (i);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	size_t	s1_len;
-	size_t	s2_len;
-	size_t	i;
-	size_t	i_ret;
-	char	*ret;
-
-	s1_len = ft_strlen(s1);
-	s2_len = ft_strlen(s2);
-	ret = malloc(((s1_len + s2_len) + 1) * sizeof(char));
-	if (ret == NULL)
-		return (NULL);
-	i = -1;
-	while (++i < s1_len)
-		ret[i] = s1[i];
-	i_ret = i;
-	i = -1;
-	while (++i < s2_len)
-		ret[i_ret + i] = s2[i];
-	ret[i_ret + i] = '\0';
-	return (ret);
-}
 
 /*
 Get the rest of a line after the first occurence of '\n'
@@ -109,6 +66,8 @@ char	*ft_cut_line(char *result)
 	i = 0;
 	while (result[i] != '\n' && result[i] != '\0')
 		i++;
+	if (i == 0)
+		return (NULL);
 	len = i;
 	if (result[i] == '\n')
 		len++;
@@ -118,7 +77,7 @@ char	*ft_cut_line(char *result)
 	if (!line)
 		return (NULL);
 	i = 0;
-	while (result[i] != '\n')
+	while (i < len)
 	{
 		line[i] = result[i];
 		i++;
@@ -127,42 +86,57 @@ char	*ft_cut_line(char *result)
 	return (line);
 }
 
-char	*get_next_line(int fd)
+char *ft_read(int fd, char *result)
 {
-	static char	*result;
-	int			read_i;
 	char		buffer[BUFFER_SIZE + 1];
 	char		*tmp;
-	char		*line;
+	int			read_i;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
 	read_i = 1;
-	if (result == NULL)
-	{
-		result = malloc(1);
-		if (result == NULL)
-			return (NULL);
-		result[0] = '\0';
-	}
 	while ((result == NULL || !contains('\n', result)) && read_i > 0)
 	{
 		read_i = read(fd, buffer, BUFFER_SIZE);
 		if (read_i == -1)
 		{
 			free(result);
-			result = NULL;
 			return (NULL);
 		}
 		buffer[read_i] = '\0';
 		tmp = result;
-		result = ft_strjoin(tmp, buffer);
-		free(tmp);
+		if (result == NULL)
+			result = ft_strdup(buffer);
+		else
+			result = ft_strjoin(tmp, buffer);
+		if (result == NULL)
+		{
+			free(tmp);
+			return (NULL);
+		}
+		if (tmp != NULL)
+			free(tmp);
 	}
-	if (read_i == -1)
+	return (result);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*result;
+	char		*tmp;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	result = ft_read(fd, result);
+	if (result == NULL)
 		return (NULL);
 	tmp = result;
 	line = ft_cut_line(result);
+	if (line == NULL)
+	{
+		free (tmp);
+		result = NULL;
+		return (NULL);
+	}
 	result = get_remainder(tmp);
 	free(tmp);
 	return (line);
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,13 @@
 /*   By: eberling <eberling@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/06 09:40:12 by eberling          #+#    #+#             */
-/*   Updated: 2025/11/12 08:46:57 by eberling         ###   ########.fr       */
+/*   Updated: 2025/11/12 09:47:46 by eberling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE
 # define GET_NEXT_LINE
 
-# define BUFFER_SIZE 5
 
 # include <fcntl.h>
 # include <stdarg.h>
@@ -22,5 +21,9 @@
 # include <stdlib.h>
 # include <unistd.h>
 char	*get_next_line(int fd);
+int		contains(char c, char *set);
+int		ft_strlen(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s);
 
 #endif
