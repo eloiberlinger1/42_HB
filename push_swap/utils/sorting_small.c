6c4c5474347a3161d9ff4ec7c8f28dc850735202
Eloi Berlinger <eloi.berling@protonmail.com>
2025-12-09 21:09:37 +0100

    debugged (pointers issue)

--- push_swap/utils/sorting_small.c
+++ push_swap/utils/sorting_small.c
@@ -6,7 +6,7 @@
 /*   By: eberling <eberling@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 10:47:34 by eberling          #+#    #+#             */
-/*   Updated: 2025/12/09 20:55:44 by eberling         ###   ########.fr       */
+/*   Updated: 2025/12/09 21:09:10 by eberling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,28 +17,27 @@ static void sort_three_elmts(t_list **current)
     t_list *elt;
 
     elt = *current;
-
     if (elt->content > elt->next->content && elt->content > elt->next->next->content) //case [3, 1, 2]
-        ft_rotate(&elt);
+        ft_rotate(current);
     else if(elt->next->content > elt->next->next->content && elt->next->content > elt->content)
-        ft_rev_rotate(&elt);
-    
+        ft_rev_rotate(current);
+
+    elt = *current;
+
     if(elt->content > elt->next->content)
     {
-        ft_swap(&elt);
+        ft_swap(current);
     }
 }
 
 void    sort_small(t_list **a, t_list **b)
 {
-    t_list **current;
     printf("lstsize:%d\n", ft_lstsize(*a));
     printf("lstsize:%d\n", ft_lstsize(*b));
 
-    current = a;
     if (ft_lstsize(*a) <= 3)
     {
-        sort_three_elmts(current);
+        sort_three_elmts(a);
     }
     else
     {
