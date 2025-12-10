4453e22450b227fee5f10bb872d190d236f56834
Eloi Berlinger <eloi.berling@protonmail.com>
2025-12-10 10:12:46 +0100

    Implementation for 5 elements looks finished

--- push_swap/utils/sorting_small.c
+++ push_swap/utils/sorting_small.c
@@ -6,7 +6,7 @@
 /*   By: eberling <eberling@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 10:47:34 by eberling          #+#    #+#             */
-/*   Updated: 2025/12/10 09:45:49 by eberling         ###   ########.fr       */
+/*   Updated: 2025/12/10 10:11:56 by eberling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,53 +25,32 @@ static void	sort_three_elmts(t_list **current)
 		ft_rev_rotate(current);
 	elt = *current;
 	if (elt->content > elt->next->content)
-	{
 		ft_swap(current);
-	}
 }
 
-static void sort_more_three_l5(t_list **a, t_list **b)
+static void	sort_more_three_l5(t_list **a, t_list **b)
 {
 	t_list *smallest;
 	size_t a_size;
 	size_t smallest_i;
 
-	a_size = ft_lstsize(*a);
-
-	while (a_size > 3)
+	while (ft_lstsize(*a) > 3)
 	{
+		a_size = ft_lstsize(*a);
 		smallest = ft_lstgetmin(*a);
 		smallest_i = a_size - ft_lstsize(smallest);
-
-		printf("index of smallest : %d\n", (int)(smallest_i));
-		if (a_size == 4)
+		while ((*a)->content != smallest->content)
 		{
-			if ((smallest_i) == 1)
-				ft_rotate(a);
-			if ((smallest_i) == 2)
-			{
-				ft_rotate(a);
-				ft_rotate(a);
-			}
-			if ((smallest_i) == 3)
-				ft_rev_rotate(a);
-			pb(a, b);
-			sort_three_elmts(a);
-			pa(a, b);
+			if ((smallest_i) <= a_size / 2)
+				ra(a);
+			if ((smallest_i) > a_size / 2)
+				rra(a);
 		}
-		if (a_size == 5)
-		{
-			if ((smallest_i) >= 2) 
-			{
-				ft_rev_rotate(a);
-				ft_rev_rotate(a);
-			}
-		}
-		(void)b;
-		//ft_push(&smallest, b);
-		a_size--;
+		pb(a, b);
 	}
-	
+	sort_three_elmts(a);
+	while (ft_lstsize(*a) <= (int)a_size)
+		pa(a, b);
 }
 
 void	sort_small(t_list **a, t_list **b)
@@ -79,13 +58,8 @@ void	sort_small(t_list **a, t_list **b)
 	size_t	list_size;
 
 	list_size = ft_lstsize(*a);
-	printf("lstsize:%d\n", ft_lstsize(*a));
-	printf("lstsize:%d\n", ft_lstsize(*b));
-
 	if (list_size <= 3)
 		sort_three_elmts(a);
-		
 	else if (list_size > 3 && list_size <= 5)
 		sort_more_three_l5(a, b);
-
 }
