593e695908f379e62a072f1503685cefee7ee318
Eloi Berlinger <eloi.berling@protonmail.com>
2025-12-13 16:51:31 +0100

    Reorganized parse_input to have 25max lines and 5 functions

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,23 +6,23 @@
 /*   By: eberling <eberling@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/01 11:41:53 by eberling          #+#    #+#             */
-/*   Updated: 2025/12/12 18:35:47 by eberling         ###   ########.fr       */
+/*   Updated: 2025/12/13 16:33:08 by eberling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	display(t_list *list)
-{
-	t_list *current;
+// static void	display(t_list *list)
+// {
+// 	t_list *current;
 
-	current = list;
-	while (current != NULL)
-	{
-		printf("--- DEBUG LIST --- (valeur: %d | index: %d)\n", current->content, current->index);
-		current = current->next;
-	}
-}
+// 	current = list;
+// 	while (current != NULL)
+// 	{
+// 		printf("--- DEBUG LIST --- (valeur: %d | index: %d)\n", current->content, current->index);
+// 		current = current->next;
+// 	}
+// }
 
 int	ft_check_dobble(t_list *list)
 {
@@ -51,31 +51,20 @@ int	main(int argc, char **argv)
 
 	b = NULL;
 	a = parse_main(argc, argv);
-	// Recuperer les valeurs entree
-	if (ft_check_dobble(a))
+	if (ft_check_dobble(a) || a == NULL)
 	{
 		ft_printf("Error\n");
 		return (0);
 	}
-	// ft_swap(&a);
-	// ft_sswap(&a, &b);
-	// ft_push(&a, &b);
-	// ft_push(&a, &b);
-	// ft_rotate(&a);
-	// ft_rev_rotate(&a);
-	// ft_rrotate(&a);
-	// ft_rrev_rotate(&a);
-	// ft_rotate(&a);
-	// lst_affect_index(a);
-	// sort_small(&a, &b);
-	// sort_main(&a, &b);
-	
-	// display list
-	ft_printf("===============VALEURS a======================\n");
-	display(a);
-	ft_printf("===============VALEURS b======================\n");
-	display(b);
-	ft_printf("====================is sorted ? ==  %d ===========\n");
-	// 	check_sort(a));
+	lst_affect_index(a);
+	sort_small(&a, &b);
+	sort_main(&a, &b);
 }
 
+// display list
+// ft_printf("===============VALEURS a======================\n");
+// display(a);
+// ft_printf("===============VALEURS b======================\n");
+// display(b);
+// ft_printf("====================is sorted ? ==  %d ===========\n");
+// 	check_sort(a));
--- push_swap/utils/parse_input.c
+++ push_swap/utils/parse_input.c
@@ -6,26 +6,23 @@
 /*   By: eberling <eberling@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/12 18:31:42 by eberling          #+#    #+#             */
-/*   Updated: 2025/12/13 14:26:12 by eberling         ###   ########.fr       */
+/*   Updated: 2025/12/13 16:50:10 by eberling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-static void *stop_and_free(int must_free, char **args)
-{
-    ft_printf("Error\n");
-    if (must_free)
-        free(args);
-    return (NULL);
-}
 
-static void *stop_and_free_list(int must_free, char **args, t_list *a)
+/*
+freelist = 1 if the list has to be freed
+*/
+static void *stop_and_free(int must_free, char **args, t_list *a, int freelist)
 {
     ft_printf("Error\n");
     if (must_free)
         free(args);
-    // free all the list
+    if (freelist)
+       free(a);
     return (NULL);
 }
 
@@ -63,33 +60,43 @@ static char **get_args_list(int argc, char **argv, int *must_free, int *i)
     return (args);
 }
 
-t_list *parse_main(int argc, char **argv)
+static void *input_to_list(char **args, int *i, int *must_free, t_list **a)
 {
-    char **args;
-    t_list	*a;
     t_list	*temp;
-    int		p;
-    int		i;
-    int     must_free;
+    int p;
+    
+    while (args[*i])
+	{
+        p = 0;
+		while (args[*i][p])
+		{
+			if (!ft_isdigit(args[*i][p]) && args[*i][p] != '-' && args[*i][p] != '+')
+				return (stop_and_free(*must_free, args, *a, 0));
+			p++;
+		}
+		temp = ft_lstnew(ft_atoi(args[*i]));
+		if (!temp)
+			return (stop_and_free(*must_free, args, *a, 1));
+		ft_lstadd_back(a, temp);
+		(*i)++;
+	}
+    return (*args);
+}
+
+t_list	*parse_main(int argc, char **argv)
+{
+	t_list	*a;
+	char 	**args;
+	int		i;
+	int		must_free;
 
     a = NULL;
     args = get_args_list(argc, argv, &must_free, &i);
-    while (args[i])
-	{
-        p = 0;
-		while (args[i][p])
-		{
-			if (!ft_isdigit(args[i][p]) && args[i][p] != '-' && args[i][p] != '+')
-				return (stop_and_free(must_free, args));
-			p++;
-		}
-		temp = ft_lstnew(ft_atoi(args[i]));
-		if (!temp)
-			return (stop_and_free_list(must_free, args, a));
-		ft_lstadd_back(&a, temp);
-		i++;
-	}
-    if (must_free)
-        free(args);
-    return (a);
+
+	if (input_to_list(args, &i, &must_free, &a) == NULL)
+		return (NULL);
+
+	if (must_free == 1)
+		free(args);
+	return (a);
 }
