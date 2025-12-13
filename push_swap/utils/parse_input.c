7d8ad95d31e0dbca56c488029ccdd1a812acc393
Eloi Berlinger <eloi.berling@protonmail.com>
2025-12-13 14:27:23 +0100

    1 - applying norm

--- push_swap/utils/parse_input.c
+++ push_swap/utils/parse_input.c
@@ -6,7 +6,7 @@
 /*   By: eberling <eberling@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/12 18:31:42 by eberling          #+#    #+#             */
-/*   Updated: 2025/12/13 14:16:35 by eberling         ###   ########.fr       */
+/*   Updated: 2025/12/13 14:26:12 by eberling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,16 @@ static void *stop_and_free(int must_free, char **args)
     return (NULL);
 }
 
-int contains(char *s, char c)
+static void *stop_and_free_list(int must_free, char **args, t_list *a)
+{
+    ft_printf("Error\n");
+    if (must_free)
+        free(args);
+    // free all the list
+    return (NULL);
+}
+
+static int contains(char *s, char c)
 {
     while (s++)
     {
@@ -28,6 +37,30 @@ int contains(char *s, char c)
             return (1);
         }
     return (0);
+}
+
+static char **get_args_list(int argc, char **argv, int *must_free, int *i)
+{
+    char **args;
+    
+    args = NULL;
+    *must_free = 0;
+    *i = 1;
+    if (argc == 2)
+    {
+        *i = 1;
+        if (contains(argv[1], ' '))
+        {
+            *must_free = 1;
+            args = ft_split(argv[1], ' ');
+            if (args == NULL)
+                return (NULL);
+            *i = 0;
+        }
+    }
+    else
+        args = argv;
+    return (args);
 }
 
 t_list *parse_main(int argc, char **argv)
@@ -39,40 +72,20 @@ t_list *parse_main(int argc, char **argv)
     int		i;
     int     must_free;
 
-    must_free = 0;
-    i = 1;
     a = NULL;
-    args = NULL;
-	p = 0;
-    if (argc == 2)
-    {
-        i = 1;
-        if (contains(argv[1], ' '))
-        {
-            must_free = 1;
-            args = ft_split(argv[1], ' ');
-            if (args == NULL)
-                return (NULL);
-            i = 0;
-        }
-    }
-    else
-        args = argv;
-
-
+    args = get_args_list(argc, argv, &must_free, &i);
     while (args[i])
 	{
         p = 0;
 		while (args[i][p])
 		{
-            // "654" "123" "-879" "45"
-			if (!ft_isdigit(args[i][p]) && args[i][p] != '-' && args[i][p] != '+') // Ajouter pour les + ou
+			if (!ft_isdigit(args[i][p]) && args[i][p] != '-' && args[i][p] != '+')
 				return (stop_and_free(must_free, args));
 			p++;
 		}
 		temp = ft_lstnew(ft_atoi(args[i]));
 		if (!temp)
-			return (0);
+			return (stop_and_free_list(must_free, args, a));
 		ft_lstadd_back(&a, temp);
 		i++;
 	}
