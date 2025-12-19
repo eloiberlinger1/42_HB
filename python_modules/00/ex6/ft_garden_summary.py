6a9f2425f9927ece191972876852f04c36b36822
Eloi Berlinger <eloi.berling@protonmail.com>
2025-12-19 14:29:13 +0100

    finished

---
+++ python_modules/00/ex6/ft_garden_summary.py
@@ -0,0 +1,8 @@
+
+def ft_garden_summary():
+    gardenname = input("Enter garden name: ")
+    plantsnumber = input("Enter number of plants: ")
+
+    print(f"Garden: {gardenname}")
+    print(f"Plants: {plantsnumber}")
+    print("Status: Growing well!")
