a965c354927542e7527e70793df4853c365ed516
Eloi Berlinger <eloi.berling@protonmail.com>
2025-12-19 12:29:48 +0100

    UP

---
+++ ex1/ft_plot_area.py
@@ -0,0 +1,9 @@
+
+def ft_plot_area():
+    input_width = int(input("Enter width : "))
+    input_lenght = int(input("Enter lenght : "))
+
+    print("Plot area : "+str(input_lenght*input_width))
+
+
+ft_plot_area()
