02eb3a43029691ef6e8cc1ac678e8e397f8f5a13
Eloi BERLINGER <eloi.berling@protonmail.com>
2025-12-21 22:54:26 +0100

    Started module 01

---
+++ python_modules/01/ex0/ft_garden_intro.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    print("=== Welcome to My Garden ===")
+    plant = input("Plant: ")
+    height = input("Height: ")
+    age = input("Age: ")
+    print("")
+    print("=== End of Program ===")
