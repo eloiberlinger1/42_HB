6b56ca37e610ea1c92b94fb26b08a89bf879cdfb
Eloi Berlinger <eloi.berling@protonmail.com>
2026-01-06 22:17:53 +0100

    remake

--- exams/rank_02/ft_atoi_base/ft_atoi_base.c
+++ exams/rank_02/ft_atoi_base/ft_atoi_base.c
@@ -1,60 +1,56 @@
 
-#include <stdio.h>
+# include <stdio.h>
+# include <stdlib.h>
 
-int isvalid(int ch, int baselen)
+int checkbase(char c, int baselen)
 {
-	char base[]="0123456789abcdef";
-	char baseMAJ[]="0123456789ABCDEF";
+	char base[] = "0123456789abcdef";
+	char base1[] = "0123456789ABCDEF";
 	int i = 0;
 
 	while (i < baselen)
 	{
-		if (ch == base[i] || ch == baseMAJ[i])
+		if (c == base1[i] || c == base[i])
 			return (1);
 		i++;
 	}
-
 	return (-1);
 }
 
 int	ft_atoi_base(const char *str, int str_base)
 {
-	int	i = 0;
-	int inc = 0;
+	int i = 0;
 	int tt = 0;
 	int digit = 1;
 
-	if (str_base < 2 || str_base > 16)
-		return (0);
+	if (str[i] == '-')
+{
+	digit = -1;
+	i++;
+}
 
-	while (str[i] == '-')
+	while (str[i])
 	{
-		digit *= -1;
-		i++;
-	}
 
-	while(str[i])
-	{
-		if (isvalid(str[i], str_base) == -1)
+		if (checkbase(str[i], str_base) == -1)
 			return (0);
-
 
 		if (str[i] >= '0' && str[i] <= '9')
 			tt = (tt * str_base) + str[i] - '0';
-		else if (str[i] >= 'a' && str[i] <= 'f')
-			tt = (tt * str_base) + 10 + str[i] - 'a';
-		else if (str[i] >= 'A' && str[i] <= 'F')
-			tt = (tt * str_base) + 10 + str[i] - 'A';
+		else if (str[i] >= 'a' && str[i] <= 'z')
+			tt = (tt * str_base) + str[i] + 10 - 'a';
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+			tt = (tt * str_base) + str[i] + 10 - 'A';
 
 		i++;
 	}
-
 	return (tt * digit);
 }
 
+
 int main()
 {
-	char test[] = "---0123";
+	char test[] = "123";
 
-	printf("%d \n", ft_atoi_base(test, 10));
+	printf("%d\n", ft_atoi_base(test, 10));
 }
