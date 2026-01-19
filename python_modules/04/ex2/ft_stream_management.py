62c451fc58d20f287ac20d980004130e3c7ee538
Eloi Berlinger <eloi.berling@protonmail.com>
2026-01-19 22:08:09 +0100

    ex2

--- python_modules/04/ex2/ft_stream_management.py
+++ python_modules/04/ex2/ft_stream_management.py
@@ -3,12 +3,36 @@
 Docstring for ex2.ft_stream_management
 """
 
+import sys
+
 
 def ft_stream_management() -> None:
     """
     Docstring for ft_stream_management
     """
     print("=== CYBER ARCHIVES - COMMUNICATION SYSTEM ===")
+    print()
+    arch_id = input("Input Stream active. Enter archivist ID: ")
+    # ARCH_7742
+
+    sys.stdout.write("Input Stream active. Enter status report: ")
+    sys.stdout.flush()
+    stat_report = sys.stdin.readline().strip()
+    sys.stdout.write("\n")
+    # All systems nominal
+
+    try:
+        archive_stat_msg = "[STANDARD] Archive status from"
+        sys.stdout.write(f"{archive_stat_msg} {arch_id}: {stat_report}\n")
+        alert_msg = "[ALERT] System diagnostic: "
+        alert_msg += "Communication channels verified\n"
+        sys.stderr.write(alert_msg)
+
+        sys.stdout.write("[STANDARD] Data transmission complete\n\n")
+    except Exception:
+        sys.stderr.write("error")
+
+    print("Three-channel communication test successful.")
 
 
 if __name__ == "__main__":
