6063ad50bcde0d4ffbe6cf09f19581de669e4af5
Eloi Berlinger <eloi.berling@protonmail.com>
2026-01-19 22:22:34 +0100

    started ex4

---
+++ ex4/ft_crisis_response.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Docstring for ex4.ft_crisis_response
+"""
+
+
+def ft_crisis_response() -> None:
+    """
+    Docstring for ft_crisis_response
+    """
+
+
+if __name__ == "__main__":
+    ft_crisis_response()
