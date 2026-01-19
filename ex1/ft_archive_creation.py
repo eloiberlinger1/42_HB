59b949daef43ba2bcff5c5428ad482155ff45095
Eloi Berlinger <eloi.berling@protonmail.com>
2026-01-19 21:52:52 +0100

    ex1 done, starting ex2...

--- ex1/ft_archive_creation.py
+++ ex1/ft_archive_creation.py
@@ -12,24 +12,26 @@ def ft_archive_creation() -> None:
     print("=== CYBER ARCHIVES - PRESERVATION SYSTEM ===")
     print("Initializing new storage unit: ")
     entries = [
-        "[ENTRY 001] New quantum algorithm discovered",
-        "[ENTRY 002] Efficiency increased by 347%",
-        "[ENTRY 003] Archived by Data Archivist trainee"
+        "[ENTRY 001] New quantum algorithm discovered\n",
+        "[ENTRY 002] Efficiency increased by 347%\n",
+        "[ENTRY 003] Archived by Data Archivist trainee\n"
     ]
     try:
-        file = open(file_name, "w")
-        print("Storage unit created successfully...")
-        print()
+        with open(file_name, "w") as file:
+            print("Storage unit created successfully...")
+            print()
 
-        print("Inscribing preservation data...")
-        for e in entries:
-            print(e)
-            file.write(e)
+            print("Inscribing preservation data...")
+            for e in entries:
+                print(e, end="")
+                file.write(e)
 
-        print("SECURE PRESERVATION:")
-        print("[CLASSIFIED] New security protocols archived")
-        print("Vault automatically sealed upon completion")
-        file.close()
+            print()
+            print("SECURE PRESERVATION:")
+            print("[CLASSIFIED] New security protocols archived")
+            print("Vault automatically sealed upon completion")
+            file.close()
+            print(f"Archive '{file_name}' ready for long-term preservation.")
     except Exception:
         print("Failed to create storage unit")
 
---
+++ ex2/ft_stream_management.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Docstring for ex2.ft_stream_management
+"""
+
+
+def ft_stream_management() -> None:
+    """
+    Docstring for ft_stream_management
+    """
+    print("=== CYBER ARCHIVES - COMMUNICATION SYSTEM ===")
+
+
+if __name__ == "__main__":
+    ft_stream_management()
