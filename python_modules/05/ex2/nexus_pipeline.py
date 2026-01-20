c16126b9d82741845fce40bcd7b55a1ad58a5616
Eloi Berlinger <eloi.berling@protonmail.com>
2026-01-20 23:08:22 +0100

    UP

---
+++ python_modules/05/ex2/nexus_pipeline.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Docstring for ex2.nexus_pipeline
+"""
+
+
+def nexus_pipeline() -> None:
+    """
+    Docstring for nexus_pipeline
+    """
+    print("test")
+
+
+if __name__ == "__main__":
+    nexus_pipeline()
