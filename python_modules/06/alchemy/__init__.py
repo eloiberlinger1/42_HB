df18420fb04cb138cd8e7622738e334e3f5d038a
Eloi BERLINGER <eloi.berling@protonmail.com>
2026-06-09 19:48:33 +0200

    distillation done

--- python_modules/06/alchemy/__init__.py
+++ python_modules/06/alchemy/__init__.py
@@ -1,5 +1,7 @@
 from .elements import create_air
+from .potions import healing_function as heal
+from .potions import strength_potion
 
-__all__ = ['create_air']
+__all__ = ['create_air', 'heal']
 __version__ = "1.0.0"
 __author__ = "Master Pythonicus"
---
+++ python_modules/06/alchemy/potions.py
@@ -0,0 +1,10 @@
+from alchemy.elements import create_air, create_earth
+from elements import create_fire, create_water
+
+
+def healing_function():
+    return f"Healing potion brewed with {create_air()} and {create_earth()}"
+
+
+def strength_potion():
+    return f"Strength potion brewed with {create_fire()} and {create_water()}"
