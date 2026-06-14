f8d789811743a3d55acaae380363e4c51bb71307
Eloi BERLINGER <eloi.berling@protonmail.com>
2026-06-14 20:46:35 +0200

    save for ex2

---
+++ python_modules/07/tournament.py
@@ -0,0 +1,34 @@
+from ex1 import (
+    HealingCreatureFactory,
+    TransformCreatureFactory,
+    HealCapability,
+    TransformCapability
+)
+from ex0 import (
+    CreatureFactory,
+    FireCreatureFactory,
+    WaterCreatureFactory
+)
+
+
+heal_fact = HealingCreatureFactory()
+transfo_fact = TransformCreatureFactory()
+flame_fact = FireCreatureFactory()
+water_fact = WaterCreatureFactory()
+
+print("Tournament 0 (basic)")
+
+
+print("[ (Flameling+Normal), (Healing+Defensive) ]")
+
+print("*** Tournament ***")
+print("2 opponents involved")
+
+print("* Battle *")
+
+f = flame_fact.create_evolved_creature("test flame")
+print("vs.")
+h = heal_fact.create_evolved_creature("test heal")
+
+print("now fight!")
+
